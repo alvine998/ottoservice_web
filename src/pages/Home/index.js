a05ad84707949b0b1ea7ca737c405c0a78671dcd
alvinecom2018@gmail.com <alvinecom2018@gmail.com>
2022-04-21 08:56:15 +0700

    first boilerplate

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navhome from '../../components/Navbar/Navhome';
+
+const Home = (props) => {
+    return(
+        <div>
+            <Navhome/>
+        </div>
+    )
+}
+
+export default Home;
